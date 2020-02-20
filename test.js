6c4880b1189b04e40ecb76e749eb3048d2faa28f
Shin Woo Lee <vriz@vriz.net>
2020-02-20 02:46:02 +0000

    remove c

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 var a = 1;
 var b = 2;
-var c = 3;
 
-function multiply(x, y, z) {
+function multiply(y, z) {
   return x * y * z;
 }
 
-var m = multiply(a, b, c);
+var m = multiply(a, b);
 console.log(m);
