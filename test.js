6a27576eac26a42774401c68030bfa64fb2dd08f
Shin Woo Lee <vriz@vriz.net>
2020-02-21 01:55:06 +0000

    add e

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ var a = 1;
 var b = 2;
 var c = 3;
 var d = 4;
+var e = 5;
 
-function multiply(w, x, y, z) {
-  return w * x * y * z;
+function multiply(v, w, x, y, z) {
+  return v, w * x * y * z;
 }
 
-var m = multiply(a, b, c, d);
+var m = multiply(a, b, c, d, e);
 
 console.log(m);
